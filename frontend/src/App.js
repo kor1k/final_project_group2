aa7796dcdad24c97d93df35929a7336a4e9d7b8f
kor1k <koortttt@gmail.com>
2019-03-09 15:02:02 +0200

    @kor1k | cleaning frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>
-                    hello world
-                </h1>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <h1>
+          hello world
+        </h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
