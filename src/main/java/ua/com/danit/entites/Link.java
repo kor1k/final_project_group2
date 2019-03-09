49a7bf08ad80458a1057f8df0a858260654fb25b
yamnyk <notAunicorN2018>
2019-03-09 13:48:38 +0200

    @yamnyk | update pom.xml h2 db, edit some entities

--- src/main/java/ua/com/danit/entites/Link.java
+++ src/main/java/ua/com/danit/entites/Link.java
@@ -2,7 +2,12 @@ package ua.com.danit.entites;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Data
 @Entity
--- src/main/java/ua/com/danit/entites/Talent.java
+++ src/main/java/ua/com/danit/entites/Talent.java
@@ -1,11 +1,11 @@
 package ua.com.danit.entites;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
