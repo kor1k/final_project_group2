8905d59cf26b2012dd1b5352383acdbe926ce80d
erom-dm <32650878+erom-dm@users.noreply.github.com>
2019-03-16 12:40:30 +0200

    @erom-dm | Add Redux Logger (#21)
    * @erom-dm | Redux DevTools plugged
    
    * @erom-dm | Redux Logger plugged and tested
    
    * @erom-dm | Test action and reducer cleaned up
    
    * Update default.js
    
    * @erom-dm | ESLint fixes
    
    * @erom-dm | reducer fix

--- frontend/src/reducers/default.js
+++ frontend/src/reducers/default.js
@@ -2,6 +2,19 @@
 // It is recommended to remove when there will be
 // at least one working reducer.
 
-const defaultReducer = (state = {}, action) => state
+const defaultReducer = (state = {}, action) => {
+/*
+  switch (action.type) {
+    case TEST_ACTION:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+*/
+  return state
+}
 
 export default defaultReducer
