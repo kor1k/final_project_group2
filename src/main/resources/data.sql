771f091d9e6c893997ef62a9e211043b1adb9e55
yamnyk <notAunicorN2018>
2019-03-19 16:40:03 +0200

    @yamnyk | data.sql -> WIP

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,7 +10,7 @@ insert into LOCATIONS values (002 , 'Berlin' , 'Germany');
 insert into LOCATIONS values (003 , 'Kyiv' , 'Ukraine');
 insert into TALENTS (ID , EMAIL , FULL_NAME , PASSWORD , LOCATION_ID) values (1 , 'email@nomail.com' , 'Toto Cutunio' , '123456789' , 001);
 insert into TALENTS (ID , EMAIL , FULL_NAME , PASSWORD , LOCATION_ID) values (2 , 'email_1@nomail.com' , 'Dodo Dududnio' , '987654321' , 002);
-insert into COMPLANIES values (011 , '' , '' , 'IT' , 'no logo' , 'Small Group inc.');
+insert into COMPANIES values (011 , '' , '' , 'IT' , 'no logo' , 'Small Group inc.');
 insert into EMPLOYERS values (111 , 'Gregory Laurence' , '1223334444' , '+1235588770' , 'workemail@nomail.com' , 011 , 003);
 insert into PROFILES values (11 , 3000 , 1);
 insert into PROFILES values (12 , 2874 , 2);
