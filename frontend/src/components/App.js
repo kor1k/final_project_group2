bab1773f75381f1b9884d279f1628a227189e056
kor1k <koortttt@gmail.com>
2019-03-19 12:25:04 +0200

    @kor1k | test commit for new pull req

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className='center'>
         {/* Only for example, delete anywhere */}
         <MainPage name='Mango'/>
+        <h1>this button stay here just for test you can delete it</h1>
         <Button>sign in</Button>
         <Button>sign up</Button>
       </div>
